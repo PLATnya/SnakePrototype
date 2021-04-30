780fc4484761f04412d6848d733775092c2618df
V <platinumcool777@gmail.com>
2021-04-30 17:46:54 +0300

    Coloring trigger, Trigger and OnlyTrigger layers

---
+++ Assets/Scripts/RoadTrigger.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RoadTrigger : MonoBehaviour
+{
+    public Color newColor;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameManager.SnakePlayer.snakeColor = newColor;
+        if (other.CompareTag("Player"))
+        {
+            foreach (Transform tailObject in GameManager.SnakePlayer.tailObjects)
+            {
+                Renderer tailRender = tailObject.GetComponent<Renderer>();
+                tailRender.material.SetVector("Vector3_pos", transform.position);
+                tailRender.material.SetColor("Color_cover",newColor);
+            }
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.CompareTag("Player") || other.CompareTag("Tail"))
+        {
+            Renderer tailRender = other.GetComponent<Renderer>();
+            tailRender.material.SetColor("Color_base", tailRender.material.GetColor("Color_cover"));
+        }
+    }
+}
